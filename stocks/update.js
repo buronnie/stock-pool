5b8736efbafda09cac4562ab4ae27d624714c6fe
kaiguo <kai.guo@mycase.com>
2017-11-18 20:16:10 -0800

    add cors support in lambda functions

--- stocks/update.js
+++ stocks/update.js
@@ -44,6 +44,10 @@ export function update(event, context, callback) {
 
     const response = {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      },
       body: JSON.stringify(params.Attributes),
     };
     callback(null, response);
